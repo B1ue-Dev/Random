089e93c8521aa3d9631693c8bc6388bdd8056f41
Blue <60958064+Jimmy-Blue@users.noreply.github.com>
2021-08-15 13:37:17 +0700

    Delete psutil.py

--- psutil.py
+++
@@ -1,7 +0,0 @@
-import psutil
-
-# A very basic CPU, Memory and Disk managament.
-
-print(f"CPU Usage: {psutil.cpu_percent()}%")
-print(f"RAM Usage: {psutil.virtual_memory().percent}%")
-print(f"Disk usage: {psutil.disk_usage('/').percent}%")
