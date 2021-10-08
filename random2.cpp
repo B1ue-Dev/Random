63f6fe995644100f949bc80030ebbacb348feb8d
Blue <60958064+Jimmy-Blue@users.noreply.github.com>
2021-10-08 17:28:16 +0700

    Create random2.cpp

---
+++ random2.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  cout << "The size of char is: " << sizeof(char) << " bytes" << endl;
+  cout << "The size of short is: " << sizeof(short) << " bytes" << endl;
+  cout << "The size of float is: " << sizeof(float) << " bytes" << endl;
+  // This will print the size of these variable
+}
