0fe7d8efd68b3a58c930c02d7dca96817e62afc6
Blue <60958064+Jimmy-Blue@users.noreply.github.com>
2022-01-26 20:36:44 +0700

    .

--- C++/ex1.cpp
+++ C++/ex1.cpp
@@ -3,39 +3,72 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cout << "Nhap so phan tu n: ";
-    cin >> n;
-    int a[n];
-    cout << "Nhap du lieu: " << endl;
-    for (int i=1; i<=n; i++)
-    {
-        cout << "a[" << i << "]= ";
-        cin >> a[i];
-    }
-    cout << "\nXuat mang: " << endl;
-    for (int i=1; i<=n; i++)
-        cout << a[i] << " ";
-    int S = 0;
-    for (int i=1; i<=n; i++)
-        S += a[i];
-    cout << "Tong la: " << S << endl;
+	int n;
+	cout << "Nhap so phan tu n: ";
+	cin >> n;
+	int a[n];
+	cout << "Nhap du lieu: " << endl;
+	
+	// Xuat mang
+	for (int i = 1; i <= n; i++)
+	{
+		cout << "a[" << i << "]= ";
+		cin >> a[i];
+	}
+	cout << "\nXuat mang: " << endl;
+	for (int i = 1; i <= n; i++)
+		cout << a[i] << " ";
 
-    int max = 0;
-    for (int i=1; i<=n; i++)
-        if (a[i] > max)
-            max = a[i];
-    cout << endl;
-    cout << "Gia tri lon nhat la: " << max << endl;
+	// Tong cua mang
+	int S = 0;
+	for (int i = 1; i <= n; i++)
+		S += a[i];
+	cout << "\n\nTong la: " << S << endl;
 
-    int min = INT_MAX;
-    for (int i = 1; i<=n; i++)
-        if (a[i] < min)
-            min = a[i];
-    cout << endl;
-    cout << "Gia tri nho nhat la: " << min << endl;
+	// Gia tri lon nhat
+	int max = 0;
+	for (int i = 1; i <= n; i++)
+		if (a[i] > max)
+			max = a[i];
+	cout << endl;
+	cout << "Gia tri lon nhat la: " << max << endl;
 
-    int am = 0, duong = 0, ko = 0;
+	// Gia tri nho nhat
+	int min = INT_MAX;
+	for (int i = 1; i <= n; i++)
+		if (a[i] < min)
+			min = a[i];
+	cout << endl;
+	cout << "Gia tri nho nhat la: " << min << endl;
 
-    return 0;
+	// Tinh am duong
+	int am = 0, duong = 0, ko = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		if (a[i] < 0)
+			am++;
+		else if (a[i] > 0)
+			duong++;
+		else if (a[i] == 0)
+			ko++;
+	}
+	cout << endl;
+	cout << "So phan tu am la: " << am << endl;
+	cout << "So phan tu duong la: " << duong << endl;
+	cout << "So phan tu khong la: " << ko << endl;
+
+	// Tinh chan le
+	int chan = 0, le = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		if (a[i] % 2 == 0)
+			chan++;
+		else
+			le++;
+	}
+	cout << endl;
+	cout << "So phan tu chan la: " << chan << endl;
+	cout << "So phan tu le la: " << le << endl;
+
+	return 0;
 }
