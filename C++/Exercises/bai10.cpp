9c58179c21e86fa6426264246ef63c75837b70b5
Jimmy-Blue <60958064+Jimmy-Blue@users.noreply.github.com>
2022-07-31 17:07:42 +0700

    feat: Sum of divisors

--- C++/Exercises/bai10.cpp
+++ C++/Exercises/bai10.cpp
@@ -1,29 +1,23 @@
 #include <iostream>
-#include <math.h>
-#include <vector>
 using namespace std;
 
 /*
-F(6) = |6-1-2-3| = 0
-F(7) = |7-1-2-3-4| = -3
+Sum of divisors.
+
+Example:
+X = 12 -> sum(X): 1 + 2 + 3 + 4 + 6 + 12 = 28
 */
 
-
 int main() {
-	int X;
+	int X, tong = 0;
 	cin >> X;
-	vector<int> a;
-	
-	for (int i = 0; i <= X; i++) {
+
+	for (int i = 0; i <= X/2; i++) {
 		if (i != 0 && X % i == 0) {
-			a.push_back(i);
+			tong += i;
 		}
 	}
-	cout << "We are here." << endl;
-	for (int i = 0; i < a.size(); i++) {
-		cout << a[i] << endl;
-	}
-	/*
-	UNDER CONSTRUCTION
-	*/
+	cout << tong + X << endl;
+
+	return 0;
 }
