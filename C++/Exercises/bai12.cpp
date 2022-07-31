7a5599c9a1702abff69b41040bcf7c60b8a2d178
Jimmy-Blue <60958064+Jimmy-Blue@users.noreply.github.com>
2022-07-31 17:09:35 +0700

    fix: Wrong exercises

--- C++/Exercises/bai12.cpp
+++ C++/Exercises/bai12.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int tong = 0;
-    int n;
-    cin >> n;
+/*
+Sum of divisors.
 
-    for (int i = 1; i <= n; i++) {
-        if (n % i == 0) {
+Example:
+X = 12 -> sum(X): 1 + 2 + 3 + 4 + 6 + 12 = 28
+*/
+
+int main() {
+    int X, tong = 0;
+    cin >> X;
+
+    for (int i = 0; i <= X/2; i++) {
+        if (i != 0 && X % i == 0) {
             tong += i;
         }
     }
+    cout << tong + X << endl;
 
-    cout << tong;
+    return 0;
 }
